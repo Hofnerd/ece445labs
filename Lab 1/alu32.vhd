75a17aed712daa605370cac196eafa74865dffcb
unknown <johnmrushing@gmail.com>
2018-06-08 10:54:45 -0400

    Updated the ALU control signal nomenclature to exactly match the lab report. Fixed testbench item 10 to exactly match lab report.

--- Lab 1/alu32.vhd
+++ Lab 1/alu32.vhd
@@ -1,42 +1,12 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06/05/2018 06:11:26 PM
--- Design Name: 
--- Module Name: alu32 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity alu32 is
     Port ( A : in STD_LOGIC_vector(31 downto 0);
            B : in STD_LOGIC_vector(31 downto 0);
-           ALUCtl : in STD_LOGIC_vector(3 downto 0);
+           ALUCntl : in STD_LOGIC_vector(3 downto 0);
            ALUout : inout STD_LOGIC_vector(31 downto 0);
            Carryin : in STD_LOGIC;
            Zero : out STD_LOGIC;
@@ -53,7 +23,7 @@ begin
 sum <= ('0'&A) + ('0'&B);
 diff <= ('0'&A) - ('0'&B);
 
-with ALUCtl select
+with ALUCntl select
     ALUout <= (A and B) when "0000",
             (A or B) when "0001",
             (A xor B) when "0011",
@@ -62,7 +32,7 @@ with ALUCtl select
             (A nor B) when "1100",
             (A and B) when others;
 
-with ALUCtl select
+with ALUCntl select
     carryout <= sum(32) when "0010",
                 diff(32) when "0110",
                 'Z' when others;
@@ -71,7 +41,7 @@ with ALUout select
     zero <= '1' when "00000000000000000000000000000000",
     '0' when others;
     
-with ALUctl select
+with ALUcntl select
     overflow <= (not(A(31)) and not(B(31)) and ALUout(31)) or (A(31) and B(31) and not(ALUout(3))) when "0010",
                 (not(A(31)) and B(31) and ALUout(31)) or (A(31) and not(B(31)) and not(ALUout(31))) when "0110",
                 'Z' when others;
--- Lab 1/alu32_tb.vhd
+++ Lab 1/alu32_tb.vhd
@@ -1,35 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06/07/2018 03:26:20 PM
--- Design Name: 
--- Module Name: alu32_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity alu32_tb is
 --  Port ( );
@@ -40,7 +10,7 @@ architecture Behavioral of alu32_tb is
 component alu32 is
     Port ( A : in STD_LOGIC_vector(31 downto 0);
        B : in STD_LOGIC_vector(31 downto 0);
-       ALUCtl : in STD_LOGIC_vector(3 downto 0);
+       ALUCntl : in STD_LOGIC_vector(3 downto 0);
        ALUout : inout STD_LOGIC_vector(31 downto 0);
        Carryin : in STD_LOGIC;
        Zero : out STD_LOGIC;
@@ -57,7 +27,7 @@ begin
  uut : alu32 port map ( 
                         A=>ta, 
                         B=>tb, 
-                        ALUCtl => tctl, 
+                        ALUCntl => tctl, 
                         ALUout => tout, 
                         Carryin => tci,
                         Zero => tz, 
@@ -95,7 +65,7 @@ begin
     ta <= X"00000000"; tb <= X"00000001"; tci <= 'Z'; tctl <= "0110";
     wait for 10 ns;    
     --10
-    ta <= X"F9684783"; tb <= X"F9684783"; tci <= 'Z'; tctl <= "0110";
+    ta <= X"F9684783"; tb <= X"F998D562"; tci <= 'Z'; tctl <= "0110";
     wait for 10 ns;
     --11
     ta <= X"9ABCDEDF"; tb <= X"9ABCDEFD"; tci <= 'Z'; tctl <= "1100";
@@ -103,6 +73,7 @@ begin
     --12
     ta <= X"89BCDE34"; tb <= X"C53BD687"; tci <= 'Z'; tctl <= "1111";
     wait for 10 ns;
+    wait;
 end process;
 
 end Behavioral;
