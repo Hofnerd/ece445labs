0deadf3965c7627a8b3c3255ac1e654a1d67d5c5
Spencer Lombardo <spencerlombardo96@gmail.com>
2018-06-12 16:48:39 -0400

    fixing shit, added comments

--- Lab 1/alu32.vhd
+++ Lab 1/alu32.vhd
@@ -1,42 +1,12 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06/05/2018 06:11:26 PM
--- Design Name: 
--- Module Name: alu32 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity alu32 is
     Port ( A : in STD_LOGIC_vector(31 downto 0);
            B : in STD_LOGIC_vector(31 downto 0);
-           ALUCtl : in STD_LOGIC_vector(3 downto 0);
+           ALUCntl : in STD_LOGIC_vector(3 downto 0);
            ALUout : inout STD_LOGIC_vector(31 downto 0);
            Carryin : in STD_LOGIC;
            Zero : out STD_LOGIC;
@@ -50,10 +20,12 @@ signal sum,diff : std_logic_vector(32 downto 0);
 
 begin
 
+--provide 33rd bit for carryout
 sum <= ('0'&A) + ('0'&B);
 diff <= ('0'&A) - ('0'&B);
 
-with ALUCtl select
+--operations as per lab spec
+with ALUCntl select
     ALUout <= (A and B) when "0000",
             (A or B) when "0001",
             (A xor B) when "0011",
@@ -62,17 +34,20 @@ with ALUCtl select
             (A nor B) when "1100",
             (A and B) when others;
 
-with ALUCtl select
+--use 33rd bit from sum or diff respectively for *unsigned* operands
+with ALUCntl select
     carryout <= sum(32) when "0010",
                 diff(32) when "0110",
                 'Z' when others;
 
+--high when answer = 0
 with ALUout select
     zero <= '1' when "00000000000000000000000000000000",
     '0' when others;
     
-with ALUctl select
-    overflow <= (not(A(31)) and not(B(31)) and ALUout(31)) or (A(31) and B(31) and not(ALUout(3))) when "0010",
+--high when we expect overflow for *signed* operands
+with ALUcntl select
+    overflow <= (not(A(31)) and not(B(31)) and ALUout(31)) or (A(31) and B(31) and not(ALUout(31))) when "0010",
                 (not(A(31)) and B(31) and ALUout(31)) or (A(31) and not(B(31)) and not(ALUout(31))) when "0110",
                 'Z' when others;
     
--- Lab 1/alu32_tb.vhd
+++ Lab 1/alu32_tb.vhd
@@ -1,38 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06/07/2018 03:26:20 PM
--- Design Name: 
--- Module Name: alu32_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity alu32_tb is
---  Port ( );
 end alu32_tb;
 
 architecture Behavioral of alu32_tb is
@@ -40,7 +9,7 @@ architecture Behavioral of alu32_tb is
 component alu32 is
     Port ( A : in STD_LOGIC_vector(31 downto 0);
        B : in STD_LOGIC_vector(31 downto 0);
-       ALUCtl : in STD_LOGIC_vector(3 downto 0);
+       ALUCntl : in STD_LOGIC_vector(3 downto 0);
        ALUout : inout STD_LOGIC_vector(31 downto 0);
        Carryin : in STD_LOGIC;
        Zero : out STD_LOGIC;
@@ -48,16 +17,18 @@ component alu32 is
        Carryout : out STD_LOGIC);
 end component;
 
+--simulated input signals
 signal ta, tb, tout : std_logic_vector(31 downto 0);
-signal tci, tco, tz, tov : std_logic;
 signal tctl : std_logic_vector(3 downto 0);
+--simulated outputs
+signal tci, tco, tz, tov : std_logic;
 
 begin
 
  uut : alu32 port map ( 
-                        A=>ta, 
-                        B=>tb, 
-                        ALUCtl => tctl, 
+                        A => ta, 
+                        B => tb, 
+                        ALUCntl => tctl, 
                         ALUout => tout, 
                         Carryin => tci,
                         Zero => tz, 
@@ -66,11 +37,12 @@ begin
 
 stimulus : process
 
+--test based on lab spec
 begin
     --1
     ta <= X"FFFFFFFF"; tb <= X"00000000"; tci <= 'Z'; tctl <= "0000";
     wait for 10 ns;
-   --2 
+    --2 
     ta <= X"98989898"; tb <= X"89898989"; tci <= 'Z'; tctl <= "0001";
     wait for 10 ns;
     --3
@@ -95,7 +67,7 @@ begin
     ta <= X"00000000"; tb <= X"00000001"; tci <= 'Z'; tctl <= "0110";
     wait for 10 ns;    
     --10
-    ta <= X"F9684783"; tb <= X"F9684783"; tci <= 'Z'; tctl <= "0110";
+    ta <= X"F9684783"; tb <= X"F998D562"; tci <= 'Z'; tctl <= "0110";
     wait for 10 ns;
     --11
     ta <= X"9ABCDEDF"; tb <= X"9ABCDEFD"; tci <= 'Z'; tctl <= "1100";
@@ -103,6 +75,9 @@ begin
     --12
     ta <= X"89BCDE34"; tb <= X"C53BD687"; tci <= 'Z'; tctl <= "1111";
     wait for 10 ns;
+    --reset
+    ta <= X"00000000"; tb <= X"00000000"; tci <= 'Z'; tctl <= "0000";
+    wait;
 end process;
 
 end Behavioral;
