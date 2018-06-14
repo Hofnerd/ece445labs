57f4281c1fa551db90eb7b8a14d3e9ef9392cb34
Spencer Lombardo <spencerlombardo96@gmail.com>
2018-06-14 17:13:53 -0400

    Started getting the PC ready

--- Lab 2/pc.vhd
+++ Lab 2/pc.vhd
@@ -2,11 +2,36 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity pc is
-
+    port ( 
+            Din : in std_logic_vector(31 downto 0);
+            reset, clk : in std_logic;
+            Dout: inout std_logic_vector(31 downto 0) );
 end pc;
 
 architecture behavior of pc is
 
+component DFFwEnable is
+    Port ( D : in STD_LOGIC;
+           En : in STD_LOGIC;
+           clk : in STD_LOGIC;
+           Q : inout STD_LOGIC );
+end component;
+
+signal en : std_logic;
+
 begin
+
+en <= '1';
+
+
+gen_reg :
+
+for i in 0 to 31 generate
+regx : DFFwEnable Port map (D => Din(i),
+                            clk => clk,
+                            En => en,
+                            Q => Dout(i));
+                            
+end generate;
 
 end behavior;
