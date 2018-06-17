80c13d9bf9ebfc8e71a869d91f1c19e5b7688e04
unknown <johnmrushing@gmail.com>
2018-06-17 16:48:28 -0400

    added alucntl

--- Lab 2/ALUcntl.vhd
+++ Lab 2/ALUcntl.vhd
@@ -2,8 +2,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity ALUcntl is
-    Port ( Fn   : in STD_LOGIC;
-           Cntl : out STD_LOGIC);
+    Port ( Fn   : in STD_LOGIC_VECTOR (4 downto 0);
+           Cntl : out STD_LOGIC_VECTOR (3 downto 0);
 end ALUcntl;
 
 architecture Behavioral of ALUcntl is
