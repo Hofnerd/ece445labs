47ffed02721c705899ba0e8980a5d75eab97512d
Spencer Lombardo <spencerlombardo96@gmail.com>
2018-06-19 15:47:06 -0400

    control skeleton added

---
+++ Lab 2/control.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity control is
+	port (	Din : in std_logic_vector( 5 downto 0);
+		dout : out std_logic);
+end control;
+
+architecture behavior of control is
+
+begin
+
+end behavior
